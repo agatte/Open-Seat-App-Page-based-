dd7044cb4dec447a8da381cbc68a8ea52b57a1b6
Anthony <gattea@xavier.edu>
2018-11-16 03:22:45 -0500

    Initial Commit

---
+++ Open Seat App (Page based)/DataViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DataViewController.swift
+//  Open Seat App (Page based)
+//
+//  Created by Anthony Gatte on 11/16/18.
+//  Copyright © 2018 Anthony Gatte. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
